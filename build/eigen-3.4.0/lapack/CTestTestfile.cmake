cc2ec568730a4faa4adc9baa24f2ed1f645751cd
Nirmal Dahal <nirmaldahal25@gmail.com>
2023-08-15 22:13:39 +0545

    added gitignore

--- build/eigen-3.4.0/lapack/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/nirma/Desktop/Computer Graphics/eigen-3.4.0/lapack
-# Build directory: C:/Users/nirma/Desktop/Computer Graphics/build/eigen-3.4.0/lapack
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
